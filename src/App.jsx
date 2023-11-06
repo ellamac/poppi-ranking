c6e084c10cf32ce5c7bcadcef582bd33c06d928b
Ella Mäkelä <ella@makela.co>
2023-11-06 20:00:20 +0200

    updating paths

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ const App = (props) => {
     createRoutesFromElements([
       <Route
         id='root'
-        path='/poppi-ranking'
+        path='/'
         element={<Layout />}
         loader={() => tracks || null}
       >
